e42f19b50017f64ab721879cd9506b83ef32883b
pranit sarode <pranitsarode2003@gmail.com>
2024-05-29 19:37:30 +0530

    1 msg

--- src/main/java/com/asterisc/in/pack/controller/HomeController.java
+++ src/main/java/com/asterisc/in/pack/controller/HomeController.java
@@ -7,50 +7,46 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.asterisc.in.pack.model.Student;
 
+import repo.StudentRrpo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
 @Controller
 public class HomeController {
 	
 	
+	@Autowired
+	StudentRrpo db;
+	
 	@RequestMapping("/")
-	String fun1()
+	String fun(Model m)
 	{
-		return "page1.html";
-	}
-
-	
-	@RequestMapping("/test")
-	String fun2(Model m)
-	{
-		String msg="hello studnet";
+		Student s=new Student();
 		
-		m.addAttribute("mymsg", msg);
-		
-		return "page2.html";
-	}
-	
-	@RequestMapping("/test2")
-	String fun3(Model m)
-	{
-		Student s=new Student(101, "ram", "nagpur", "78.3");
+		System.out.println("before send to page "+s);
 		
 		m.addAttribute("obj",s);
 		
-		return "page3.html";
+		return "register.html";
 	}
-	
-	@RequestMapping("/test3")
-	ModelAndView fun4()
+
+	@RequestMapping("/reg")
+	@ResponseBody
+	String fun2(@ModelAttribute Student s)
 	{
+		System.out.println("After from Register page Submit to page "+s);
 		
-		ModelAndView mv=new ModelAndView("page4.html");
+		db.save(s);
 		
-		Student s=new Student(101, "ram", "nagpur", "78.3");
 		
-		mv.addObject("obj", s);
 		
-		return mv;
+		return "Student Register";
 	}
-	
-	
-	
 }
--- src/main/java/com/asterisc/in/pack/model/Student.java
+++ src/main/java/com/asterisc/in/pack/model/Student.java
@@ -1,6 +1,16 @@
 package com.asterisc.in.pack.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "stud_spring")
 public class Student {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 private int roll;
 private String name;
 private String address;
